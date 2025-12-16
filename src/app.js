867af77f535c05dff8c4adf777f36a710a47e01d
Miguel Anxo Figueirido Prado <miguelfigueiridoprado@gmail.com>
2025-12-16 17:57:03 +0100

    Add logging and security middlewares.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
 const dbReady = require('./middleware/dbReady');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
@@ -8,14 +13,24 @@ const swaggerSpec = require('./swagger');
 const authRoutes = require('./routes/authRoutes');
 const recipeRoutes = require('./routes/recipeRoutes');
 
+// Logging middleware
+app.use(morgan('dev'));
+
+// Security middlewares
+app.use(helmet());
+app.use(cors());
+
+// Body parser middleware
 app.use(express.json());
 
+// Rate limiting middleware
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 200 });
+app.use(limiter);
+
+// Routes
 app.use('/api/auth', dbReady, authRoutes);
-
 app.use('/api/recipes', dbReady, recipeRoutes);
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 app.get('/', (req, res) => res.json({ message: 'Simple REST API using Express.js' }));
 
 module.exports = app;
